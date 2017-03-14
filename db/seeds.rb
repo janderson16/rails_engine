d461c0e547aa86a6db2da94392dad016293c46c1
James Anderson <james.p.anderson16@gmail.com>
2017-03-14 15:42:39 -0600

    cleaned up tests for customers, corrected csv import for customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,11 @@
 require 'csv'
-# merchant_file = './data/merchants.csv'
-# merchant = File.read(merchant_file)
-# merchant_cells = CSV.parse(merchant, headers: true)
-# merchant_cells.each do |row|
-#   Merchant.create!(
-#       id:         row[:id],
-#       name:       row[:name],
-#       created_at: row[:created_at],
-#       updated_at: row[:updated_at]
-#                   )
-#   end
+
 Merchant.destroy_all
 CSV.foreach("./data/merchants.csv", :headers => true) do |row|
   Merchant.create!(row.to_hash)
+end
+
+Customer.destroy_all
+CSV.foreach("./data/customers.csv", :headers => true) do |row|
+  Customer.create!(row.to_hash)
 end
