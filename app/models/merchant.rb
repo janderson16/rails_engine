262732c9083b8fb4b9061382c1321d7233132a8d
James Anderson <james.p.anderson16@gmail.com>
2017-03-15 15:24:25 -0600

    added rank merchants by revenue method/test; not passing spec harness yet

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,15 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :transactions, through: :invoices
+  has_many :invoice_items, through: :invoices
   has_many :customers, through: :invoices
+
+  def self.most_revenue(count)
+    joins(:transactions, :invoice_items)
+    .where(transactions: {result: "success"})
+    .group(:id).order("sum(quantity * unit_price) DESC")
+    .limit(count)
+  end
 
 end
