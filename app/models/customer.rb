9b74c6970f98f605c5556ef7c9effc3b6b4c3e4f
James Anderson <james.p.anderson16@gmail.com>
2017-03-14 18:33:49 -0600

    removed obsolete date formatting in models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,11 +1,4 @@
 class Customer < ApplicationRecord
   has_many :invoices
 
-  def formatted_create
-    created_at.iso8601(3)
-  end
-
-  def formatted_update
-    updated_at.iso8601(3)
-  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,11 +2,4 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
 
-  def formatted_create
-    created_at.iso8601(3)
-  end
-
-  def formatted_update
-    updated_at.iso8601(3)
-  end
 end
